83c08a1c6b5a1af50d57fb8e75366b98c6609ca2
Mike McTighe <m.mctighe10@gmail.com>
2019-09-11 16:29:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,7 @@
 # Write your code here.
-def badge_maker(og_array)
-  new_array = []
-  og_array.each do |fillout|
-new_array
- end
-end
 
 name =  gets.chomp
 def badge_maker(name)
   bage_maker.each do |make|
     puts "Hello, my name is #{name}."
-  end 
+  end
