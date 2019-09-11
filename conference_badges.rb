2ce8f8e4a8fc67427754713422fb18d5b7744f9f
Mike McTighe <m.mctighe10@gmail.com>
2019-09-11 16:56:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 
 def assign_rooms(array)
 array.each_with_index  do |name, index|
-  "Hello, #{name }! You'll be assigned to room 1!"
+  "Hello, #{name}! You'll be assigned to room 1!"
 
 end
 end
